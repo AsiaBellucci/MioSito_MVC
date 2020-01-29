36083e22db3aa34571d474bbec468e87b2e18af4
a.bellucci <a.bellucci@VETRYA-PC003>
2020-01-29 18:07:39 +0100

    Add project files.

---
+++ MioSito/Controllers/ContattiController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MioSito.Models;
+
+namespace MioSito.Controllers
+{
+    public class ContattiController : Controller
+    {
+
+        // GET: Contatti
+        public ActionResult Index()
+        {
+            Contatto c = new Contatto();
+            
+            return View(c);
+        }
+
+        // GET: Contatti/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Contatti/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contatti/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contatti/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Contatti/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contatti/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Contatti/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        
+    }
+}
---
+++ MioSito/Models/Contatto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MioSito.Models
+{
+    public class Contatto
+    {
+        public string Nome { get; set; } = "Giuseppe"; 
+        public string Cognome { get; set; } = "Baldoni";
+        public string Indirizzo { get; set; } = "via byhv";
+        public string Città { get; set; } = "baban";
+        public int Telefono { get; set; } = 125463;
+        public string Immagine { get; set; } = "~/images/ImagineDragons.jpg";
+
+        
+    }
+}
